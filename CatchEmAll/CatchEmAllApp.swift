48674d47fd500ebbe69816f5ee23f4f2055605eb
anorazhu <zhusm@bc.edu>
2024-10-20 19:36:22 -0400

    Initial Commit

---
+++ CatchEmAll/CatchEmAllApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatchEmAllApp.swift
+//  CatchEmAll
+//
+//  Created by Anora Zhu on 10/20/24.
+//
+
+import SwiftUI
+
+@main
+struct CatchEmAllApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CatchEmAllTests/CatchEmAllTests.swift
@@ -0,0 +1,17 @@
+//
+//  CatchEmAllTests.swift
+//  CatchEmAllTests
+//
+//  Created by Anora Zhu on 10/20/24.
+//
+
+import Testing
+@testable import CatchEmAll
+
+struct CatchEmAllTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
